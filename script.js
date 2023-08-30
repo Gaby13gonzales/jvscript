1a22d601bb24b0653748d3f94f5ad7d33ed3edc5
Gaby13gonzales <126676760+Gaby13gonzales@users.noreply.github.com>
2023-08-30 11:46:25 -0300

    commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// Variáveis
+let nomeAluno;
+let sobrenomeAluno;
+let anoNascimentoAluno;
+let idadeAluno;
+
+// Constantes
+const NOME_ESCOLA = "Senai";
+const ANO_ATUAL = 2023;
+
+// Receber informações do usuário
+nomeAluno = prompt("Digite o seu primeiro nome:");
+sobrenomeAluno = prompt("Digite o seu sobrenome:");
+let nomeAlunoCompleto = nomeAluno + " " + sobrenomeAluno;
+anoNascimentoAluno = parseInt(prompt("Digite o ano do seu nascimento:"));
+idadeAluno = ANO_ATUAL - anoNascimentoAluno;
+
+alert("Seu nome é " + nomeAlunoCompleto + " e você estuda no " + NOME_ESCOLA + ", sua idade é " + idadeAluno + " ano(s).");
